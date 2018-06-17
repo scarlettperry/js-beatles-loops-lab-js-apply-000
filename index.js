6377df2e4765f5bb0e6db6f5193a551965e8e541
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-17 00:14:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ function theBeatlesPlay(musicians, instruments)
   {
       BeatlesLoops.push(musicians[i] +" plays "+ instruments[i]);
   }
-          return BeatlesLoops;
+  return BeatlesLoops;
 }
 
 const facts =[ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
 
 function johnLennonFacts (facts){
-  var jFacts =[];
-  
+  var jFacts = [];
+  for(var i=0; i<facts.length; i++)
+  {
+    jFacts.push(facts[i] +"!!!"+);
+  }
+  return jFacts;
 }
