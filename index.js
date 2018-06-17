5331988fc4671b7e41dee6fad85cb4320bcc078f
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-17 00:03:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-var BeatlesLoops = [];
+
 
 function theBeatlesPlay(musicians, instruments){
   for(var i=0; i<musicians.length; i++){
     for (var j=0; j<instruments.length; j++){
+      var BeatlesLoops = [];
       BeatlesLoops.push(musicians[i] +" plays "+ instruments[j]);
       return BeatlesLoops;
     }
