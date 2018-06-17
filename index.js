70a1e0e673c947a8745fc4a40f494020869c54fd
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-17 00:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function theBeatlesPlay(musicians, instruments)
   for(var i=0; i<musicians.length; i++)
   {
       BeatlesLoops.push(musicians[i] +" plays "+ instruments[i]);
-      return BeatlesLoops;
     }
+          return BeatlesLoops;
+
 }
