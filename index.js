b179ef3d7da2aa98ef43c827daa282d31b89e1f5
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-17 00:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,5 @@ function theBeatlesPlay(musicians, instruments)
 }
 
 const facts =[ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+
+function 
